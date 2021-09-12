0467a372f0a11a99f33fdefe50c00acfb318b2c7
Ben Eng <whysguy52@gmail.com>
2021-09-12 02:05:06 -0500

    Oculus Quest 2 Setup
    Oculus Quest 2 Setup working

--- Main/Main.gd
+++ Main/Main.gd
@@ -1,16 +1,25 @@
 extends Spatial
 
 
+var perform_runtime_config = false
+
+
+#onready var ovr_init_config = preload("res://addons/godot_ovrmobile/OvrInitConfig.gdns").new()
+#onready var ovr_performance = preload("res://addons/godot_ovrmobile/OvrPerformance.gdns").new()
+
+
 func _ready():
-  var vr = ARVRServer.find_interface("Oculus")
+    var interface = ARVRServer.find_interface("OVRMobile")
+    if interface and interface.initialize():
+       # Tell our viewport it is the arvr viewport:
+        get_viewport().arvr = true
 
-  if vr and vr.initialize():
-    get_viewport().arvr = true
+        # change our physics fps
+        Engine.iterations_per_second = 72 # Quest
 
-    OS.vsync_enabled = true
-    
-    # NOTE: the physics FPS in the project settings is also 90 FPS
-    #       Project Settings -> General -> Physics -> Common -> Physics FPS
-    #       this makes the physics run at the same frame rate as the display
-    #       which makes things look smoother in VR
-    Engine.target_fps = 90
+
+#func _process(_delta):
+#    if not perform_runtime_config:
+#        ovr_performance.set_clock_levels(1, 1)
+#        ovr_performance.set_extra_latency_mode(1)
+#        perform_runtime_config = true
